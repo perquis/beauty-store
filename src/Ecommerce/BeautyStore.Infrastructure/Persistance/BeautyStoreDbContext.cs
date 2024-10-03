762fd766236db5ab3d80b7cabcfd669b0987215e
perquis <thetahirpl@gmail.com>
2024-10-03 19:26:13 +0200

    :bug: bugfix(DbContext): Updated encapsulation for Products attribute and replaced it from private to public to get access for that.

--- src/Ecommerce/BeautyStore.Infrastructure/Persistance/BeautyStoreDbContext.cs
+++ src/Ecommerce/BeautyStore.Infrastructure/Persistance/BeautyStoreDbContext.cs
@@ -5,16 +5,16 @@ namespace BeautyStore.Infrastructure.Persistance
 {
     public class BeautyStoreDbContext : DbContext
     {
-        private DbSet<Product> Products { get; set; } = default!;
+        public DbSet<Product> Products { get; set; } = default!;
 
         public BeautyStoreDbContext(DbContextOptions<BeautyStoreDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
+
             builder.Entity<Product>().HasKey(p => p.Id);
             builder.Entity<Product>().HasMany(p => p.Images).WithOne(p => p.Product).HasForeignKey(i => i.ProductId);
-
-            base.OnModelCreating(builder);
         }
     }
 }
