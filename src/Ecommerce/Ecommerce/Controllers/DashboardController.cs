8619a10560a7acc4af4061664d92ad41ee46e86c
perquis <thetahirpl@gmail.com>
2024-10-03 19:27:23 +0200

    :sparkles: feature(DashboardController): Created new controller to can log in to the app when the user is admin and to add for example new products and edit stock values.

---
+++ src/Ecommerce/Ecommerce/Controllers/DashboardController.cs
@@ -0,0 +1,49 @@
+﻿using Ecommerce.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using BeautyStore.Application.Product.Commands.CreateProduct;
+
+namespace Ecommerce.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ILogger<DashboardController> _logger;
+        private readonly IMediator _mediator;
+
+        public DashboardController(ILogger<DashboardController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Product/Create")]
+        public async Task<IActionResult> CreateProduct(CreateProductCommand command)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(command);
+            }
+
+            await _mediator.Send(command);
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
