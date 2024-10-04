f39a6ac8f7660abe21922d36b790c9d2b2d894db
perquis <thetahirpl@gmail.com>
2024-10-04 13:56:14 +0200

    :sparkles: feature(HomeController): Added functionality to redirect the user from HomePage to Dashboard when he is logged in the system with admin role.

--- src/Ecommerce/Ecommerce/Controllers/HomeController.cs
+++ src/Ecommerce/Ecommerce/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using BeautyStore.Application.User;
 using Ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,14 +8,23 @@ namespace Ecommerce.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUserSession _userSession;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUserSession userSession)
         {
             _logger = logger;
+            _userSession = userSession;
         }
 
         public IActionResult Index()
         {
+            var user = _userSession.GetSession();
+
+            if (user != null && user.IsInRole("Admin"))
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+
             return View();
         }
 
