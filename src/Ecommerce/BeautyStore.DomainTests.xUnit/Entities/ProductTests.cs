05055f843bc67bb866195054def037c3d00aaf6f
perquis <thetahirpl@gmail.com>
2024-10-06 16:04:44 +0200

    :white_check_mark: tests(Product): Created ProductTests to test different use cases.

---
+++ src/Ecommerce/BeautyStore.DomainTests.xUnit/Entities/ProductTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace BeautyStore.Domain.Entities.Tests
+{
+    public class ProductTests
+    {
+        [Fact()]
+        public void EncodeName_ShouldSetEncodedName()
+        {
+            var product = new Product()
+            {
+                Name = "Lipstick Limo",
+                Price = 10.99m,
+                Description = "A lipstick for your lips",
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow,
+                Currency = Currency.USD,
+                Stock = 100,
+                Category = ProductCategory.Makeup,
+                Id = Guid.NewGuid()
+            };
+
+            product.EncodeName();
+
+            product.EncodedName.Should().Be("lipstick-limo");
+        }
+
+        [Fact()]
+        public void EncodeName_ShouldThrowException_WhenNameIsNull()
+        {
+            var product = new Product()
+            {
+                Price = 10.99m,
+                Description = "A lipstick for your lips",
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow,
+                Currency = Currency.USD,
+                Stock = 100,
+                Category = ProductCategory.Makeup,
+                Id = Guid.NewGuid()
+            };
+
+            Action action = () => product.EncodeName();
+
+            action.Should().Throw<NullReferenceException>();
+        }
+
+        [Fact()]
+        public void Images_ShouldBeInitialized_WhenItIsProvided()
+        {
+            var product = new Product()
+            {
+                Name = "Lipstick Limo",
+                Price = 10.99m,
+                Description = "A lipstick for your lips",
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow,
+                Currency = Currency.USD,
+                Stock = 100,
+                Category = ProductCategory.Makeup,
+                Id = Guid.NewGuid(),
+                Images = new List<ProductImage>()
+                {
+                    new ProductImage()
+                    {
+                        Url = "https://example.com/image.jpg"
+                    }
+                }
+            };
+
+            product.Images.Should().NotBeNull();
+            product.Images.Should().HaveCount(1);
+        }
+    }
+}
---
+++ src/Ecommerce/BeautyStore.DomainTests/Entities/ProductTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using BeautyStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautyStore.Domain.Entities.Tests
+{
+    public class ProductTests
+    {
+        [Fact()]
+        public void EncodeNameTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+    }
+}
