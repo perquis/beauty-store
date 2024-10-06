933ecc967e1c9bebb07b730aa41e8105012fb90c
perquis <thetahirpl@gmail.com>
2024-10-06 17:49:28 +0200

    :white_check_mark: tests(GetUserSession): Tested this class to check whether the object can return true or not if the role is existed in the list.

---
+++ src/Ecommerce/BeautyStore.ApplicationTests.xUnit/User/GetUserSessionTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace BeautyStore.Application.User.Tests
+{
+    public class GetUserSessionTests
+    {
+        [Fact()]
+        public void IsInRoleTest_ShouldReturnTrue_WhenTheUserHasAdminOrUserRole()
+        {
+            var userSession = new GetUserSession()
+            {
+                Id = "1",
+                Email = "example@gmail.com",
+                Role = new List<string> { "Admin", "User" }
+            };
+
+            var isAdminRole = userSession.IsInRole("Admin");
+            var isUserRole = userSession.IsInRole("User");
+
+            isAdminRole.Should().BeTrue();
+            isUserRole.Should().BeTrue();
+        }
+
+        [Fact()]
+        public void IsInRoleTest_ShouldReturnFalse_WhenTheUserDoesNotHaveAdminOrUserRole()
+        {
+            var userSession = new GetUserSession()
+            {
+                Id = "1",
+                Email = "example@gmail.com",
+                Role = new List<string> { }
+            };
+
+            var isAdminRole = userSession.IsInRole("Admin");
+            var isUserRole = userSession.IsInRole("User");
+
+            isAdminRole.Should().BeFalse();
+            isUserRole.Should().BeFalse();
+        }
+    }
+}
